b597734238ff86f5059310cae939032f9763097b
chriskohlbrenner <c.kohlbrenner@gmail.com>
2015-11-21 22:22:44 -0500

    note app

--- myNoteCtrl.js
+++ myNoteCtrl.js
@@ -1,3 +1,16 @@
 noteApp.controller('myNoteCtrl', function($scope) {
-  $scope.thing1 = "hi";
+  $scope.noteSaved = false;
+  $scope.noteContent = "";
+  $scope.charsRemaining = function(){
+    return 140 - $scope.noteContent.length;
+  }
+  $scope.saveNote = function(){
+    $scope.noteSaved = true;
+  }
+  $scope.clearNote = function(){
+    $scope.noteContent = "";
+  }
+  $scope.editNote = function(){
+    $scope.noteSaved = false;
+  }
 });

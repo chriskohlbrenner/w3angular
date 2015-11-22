10dcb9645ae31fa545d9fba8e37ee7d817306881
chriskohlbrenner <c.kohlbrenner@gmail.com>
2015-11-21 21:50:26 -0500

    primative sorting on click

--- brandNew.js
+++ brandNew.js
@@ -4,5 +4,10 @@ app.controller('brandNewCtrl', function($scope, $http) {
   $http.get("http://worldcup.kimonolabs.com/api/teams")
   .success(function(response) {
     $scope.teams = response;
+    $scope.orderBy = "name";
+    $scope.sortBy = function(sort) {
+      $scope.orderBy = sort;
+    };
+
   });
 });
